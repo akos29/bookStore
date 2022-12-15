5cd4cf35874f457a63f1d97249f4ee28a4f61134
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-15 18:35:59 +0200

    Add components and configure React router with few styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Books from './components/Books';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Books />
+          }
+          />
+          <Route
+            path="/categories"
+            element={
+              <Categories />
+          }
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Books.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Books() {
+  return (
+    <div>
+      <h1>Book Store</h1>
+      <p>Welcome ...</p>
+    </div>
+  );
+}
